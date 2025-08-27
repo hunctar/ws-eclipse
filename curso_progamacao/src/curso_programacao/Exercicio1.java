34ff040c4013981e2ba8814166d138583717429f
hunctar <hunctar@gmail.com>
2025-08-27 08:36:16 -0300

    Primeiro Commit

---
+++ curso_progamacao/src/curso_programacao/Exercicio1.java
@@ -0,0 +1,34 @@
+package curso_programacao;
+import java.util.Locale;
+
+public class Exercicio1 {
+	
+public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f ", product1, price1);
+		System.out.println("");
+		System.out.printf("%s, which price is $ %.2f", product2, price2);
+		System.out.println("");
+		System.out.println("");
+		System.out.printf("Record: %d years old, code %d and gender: %s", age, code, gender);
+		System.out.println("");
+		System.out.println("");
+		System.out.printf("Measure with eight decimal places: %.8f", measure);
+		System.out.println("");
+		System.out.printf("Rouded (three decimal places): %.3f", measure);
+		System.out.println("");
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measure);
+
+	}
+}
